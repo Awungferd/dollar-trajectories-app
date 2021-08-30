c552dd9ffd172a5273b15558f870b8953ffa57ac
Ferdinand <ndimbecha@gmail.com>
2021-08-30 10:04:56 +0200

    ANother commit

--- src/anotherApp.js
+++ src/anotherApp.js
@@ -1,7 +1,7 @@
 function initMap() {
   const options = {
     center: { lat: 52.52, lng: 13.405 },
-    zoom: 17,
+    zoom:50 ,
     mapType: google.maps.MapTypeId.ROADMAP,
   };
   map = new google.maps.Map(document.getElementById("map"), options);
